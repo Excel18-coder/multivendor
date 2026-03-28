ceac360c1eac7da8ed38cd6856c83f4d6e46f639
Excel18-coder <excel2130@gmail.com>
2026-03-28 14:34:22 -0400

    Derived types using Iota

--- main.go
+++ main.go
@@ -2,23 +2,12 @@ package main
 
 import "fmt"
 
-type Rectangle struct {
-	Length float64
-	Width  float64
-}
-
-func (r Rectangle) Area() float64 {
-	return r.Length * r.Width
-}
-
-func (r *Rectangle) setLength(l float64) {
-	r.Length = l
-
-}
+type Age int
 
 func main() {
-	rect := Rectangle{Length: 20, Width: 30}
-	fmt.Println(rect.Area())
-	rect.setLength(50)
+	var young Age = 10
+	var old Age = 60
+
+	fmt.Println(young + old)
 
 }
